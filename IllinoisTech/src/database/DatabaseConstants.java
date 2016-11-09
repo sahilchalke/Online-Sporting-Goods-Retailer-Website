cf31eaf51e68041de5f9c530013b1cfafc3d8413
Dhruvit-IIT <dhruvit@104.194.112.182>
2016-11-09 16:29:37 -0600

    Database interface

---
+++ IllinoisTech/src/database/DatabaseConstants.java
@@ -0,0 +1,13 @@
+package database;
+
+public class DatabaseConstants {
+	
+		// JDBC driver name and database URL
+		static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+		static final String DB_URL = "jdbc:mysql://localhost:3306/IllinoisTech";
+
+		//  Database credentials
+		static final String USER = "root";
+		static final String PASS = "Dhruvit@mysql";
+
+}
